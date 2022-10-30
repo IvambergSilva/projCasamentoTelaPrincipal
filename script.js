9899ce5928781fbf560355176b732e4af9e40daa
Ivamberg Silva <99219836+IvambergSilva@users.noreply.github.com>
2022-10-29 21:36:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let codigoPessoas = ['K0', 'L1', 'F2', 'N3', 'T4', 'M5', 'V6', 'M7', 'M8', 'A9',
 
 let body = document.querySelector('body')
 
-//document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('contextmenu', event => event.preventDefault());
 
 var inputCodigo = document.querySelector('#inputCodigo')
 
@@ -35,6 +35,7 @@ inputCodigo.addEventListener('click', (e) => {
 })
 
 const audio = document.querySelector('audio');
+audio.currentTime = 25;
 
 let movimento = 0
 
